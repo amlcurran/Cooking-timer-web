15488317473e811095aa1e28c439ea288f1bede7
Alex Curran <aml.curran@gmail.com>
2014-12-21 22:25:09 +0000

    Add another test, which already passed D:

--- specs/TimerListInput.js
+++ specs/TimerListInput.js
@@ -6,9 +6,16 @@ describe('Inputting data into timerlist', function(){
     expect(list.getTimerName(0)).toBe('Turkey');
   });
 
-  it('should take the inputted name', function(){
+  it('should take the inputted name pt 2', function(){
     var list = new TimerList();
     list.addTimer(new Timer('Beef'));
+    expect(list.getTimerName(0)).toBe('Beef');
+  });
+
+  it('should not overwrite previous items', function(){
+    var list = new TimerList();
+    list.addTimer(new Timer('Beef'));
+    list.addTimer(new Timer('Grapes'));
     expect(list.getTimerName(0)).toBe('Beef');
   });
 
