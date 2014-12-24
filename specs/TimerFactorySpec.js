cb809c3e313015b652782e50fb4c2ee2b230d3c5
Alex Curran <aml.curran@gmail.com>
2014-12-24 16:31:49 +0000

    Refactored the tests to make adding more easier

--- specs/TimerFactorySpec.js
+++ specs/TimerFactorySpec.js
@@ -1,14 +1,16 @@
 describe('Validating a timer', function(){
 
+  var succeeded = false;
+  var failed = false;
+  var callbacks = new TimerFactoryCallback(function(timer) {
+    succeeded = true;
+  }, function() {
+    failed = false;
+  });
+
   it('should pass if there is a name supplied', function(){
-    var succeeded = false;
-    var failed = false;
     var factory = new TimerFactory();
-    var callbacks = new TimerFactoryCallback(function(timer) {
-      succeeded = true;
-    }, function() {
-      failed = false;
-    });
+
     factory.create('null', callbacks);
     expect(succeeded).toBe(true);
   });
